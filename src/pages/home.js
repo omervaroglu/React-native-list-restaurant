8f66b3d9d139c65cfd145b767b4c3b88f65cda53
Ömer Varoğlu <omervaroglu@gmail.com>
2018-04-28 05:11:35 +0300

    step3

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, ScrollView, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 
 class Home extends Component {
@@ -7,10 +8,30 @@ class Home extends Component {
     render() {
         return (
           <View style={{ flex: 1, marginTop: 50, }}>
-            <Text> ilk sayfa </Text>
+
+              <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.container}
+                    initialRegion={{
+                        latitude: 41.0133722,
+                        longitude: 28.9738776,
+                        latitudeDelta: 0.0122,
+                        longitudeDelta: 0.0121,
+                    }}
+              />
+
           </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        height: '40%',
+        width: '100%',
+        backgroundColor: 'black',
+        borderTopWidth: 1,
+      },
+      });
+
 
 export default Home;
